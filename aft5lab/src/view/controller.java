77aebebd9743ec1c850ebe9c40cb4b6b8e19b9fd
user <user@aft>
2018-05-26 18:46:50 +0200

    package sort

--- aft5lab/src/view/controller.java
+++ aft5lab/src/view/controller.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aft5lab;
+package view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- aft5lab/src/view/main.java
+++ aft5lab/src/view/main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aft5lab;
+package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author user
  */
-public class aft5lab extends Application {
+public class main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
